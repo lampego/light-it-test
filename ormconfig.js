939bd398695c6064455d7e9cece7252824b7c581
lampego <lampego@gmail.com>
2021-05-03 16:08:49 +0300

    #236 AzurePipelines

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,15 @@
 const isTest = process.env.NODE_ENV === 'test';
 
+let host = isTest ? 'test-database' : process.env['DB_HOST'];
+if (process.env['IS_AZURE_PIPELINES']) {
+  host = '127.0.0.1';
+}
+
 module.exports = {
   name: 'default',
   type: 'mysql',
   port: 3306,
-  host: isTest
-    ? 'test-database'
-    : process.env['DB_HOST'],
+  host: host,
   username: process.env['DB_USER'],
   password: process.env['DB_PASSWORD'],
   database: process.env['DB_DATABASE'],
